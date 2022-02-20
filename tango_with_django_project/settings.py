136a0ccaf257939d868a3273ff8b04c6265bae2f
Paul_Win <yingheting@gmail.com>
2022-02-20 17:11:55 +0000

    chapter 9 done

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = 'o_wnj^&vs4oi)7jnsuf#2t^f3)17fyu+g(tb@5-(&n4-bgivm-'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+LOGIN_URL = 'rango:login'
 
 # Application definition
 
@@ -91,6 +92,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
